6bd6006fa8d2a4ab8afd353296ac550ec702df77
Moufoulyroun Kouferidji <micromouf@yahoo.fr>
2019-02-25 07:47:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -50,6 +50,9 @@ def turn(board)
   if valid_move?(board, index) 
     move(board, index, player = "X")
   else
+    puts "Please enter 1-9:"
+   user_input = gets.strip
+   index = input_to_index(user_input)
     
 
   end   
