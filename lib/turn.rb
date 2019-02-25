6329ff1cc95268604537eccb6c391d5227d3e0b0
Moufoulyroun Kouferidji <micromouf@yahoo.fr>
2019-02-25 07:12:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -49,7 +49,8 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index) 
     move(board, index, player = "X")
-  else   
+  else 
+    
 
   end   
   display_board(board)
